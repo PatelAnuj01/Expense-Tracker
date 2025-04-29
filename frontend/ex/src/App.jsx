70dfbde6c6d165ecb42bf1d8dd5d9eeb84a578fa
Anuj Patel <patel.anujp0101@gmail.com>
2025-04-29 18:53:56 +0530

    comment

--- frontend/ex/src/App.jsx
+++ frontend/ex/src/App.jsx
@@ -7,48 +7,48 @@ import {
 } from 'react-router-dom';
 
 import Login from './pages/Auth/Login';
-import SignUp from './pages/Auth/SignUp';
-import Home from './pages/Dashboard/Home';
-import Income from './pages/Dashboard/Income';
-import Expense from './pages/Dashboard/Expense';
-import UserProvider from './context/userContext';
-import {Toaster} from 'react-hot-toast'
+// import SignUp from './pages/Auth/SignUp';
+// import Home from './pages/Dashboard/Home';
+// import Income from './pages/Dashboard/Income';
+// import Expense from './pages/Dashboard/Expense';
+// import UserProvider from './context/userContext';
+// import {Toaster} from 'react-hot-toast'
 
-const App = () => {
-  return (
-    <UserProvider>
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Root />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signUp" element={<SignUp />} />
-          <Route path="/dashboard" element={<Home />} />
-          <Route path="/income" element={<Income />} />
-          <Route path="/expense" element={<Expense />} />
-        </Routes>
-      </Router>
-    </div>
+// const App = () => {
+//   return (
+//     <UserProvider>
+//     <div>
+//       <Router>
+//         <Routes>
+//           <Route path="/" element={<Root />} />
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/signUp" element={<SignUp />} />
+//           <Route path="/dashboard" element={<Home />} />
+//           <Route path="/income" element={<Income />} />
+//           <Route path="/expense" element={<Expense />} />
+//         </Routes>
+//       </Router>
+//     </div>
 
-    <Toaster
-      toastOptions={{
-        className: "",
-        style: { fontSize: "13px"},
-      }}
-    />
+//     <Toaster
+//       toastOptions={{
+//         className: "",
+//         style: { fontSize: "13px"},
+//       }}
+//     />
 
-    </UserProvider>
-  );
-};
+//     </UserProvider>
+//   );
+// };
 
-export default App;
+// export default App;
 
-const Root = () => {
-  const isAuthenticated = !!localStorage.getItem("token");
+// const Root = () => {
+//   const isAuthenticated = !!localStorage.getItem("token");
 
-  return isAuthenticated ? (
-    <Navigate to="/dashboard" />
-  ) : (
-    <Navigate to="/login" />
-  );
-};
+//   return isAuthenticated ? (
+//     <Navigate to="/dashboard" />
+//   ) : (
+//     <Navigate to="/login" />
+//   );
+// };
